0caafbed1560dbd3487100483df9eb5ac7b41184
MarquinhosCG07 <marco.antonio.cg07@gmail.com>
2026-04-22 01:24:04 -0300

    Quinto commit

--- BACKEND/API/index.js
+++ BACKEND/API/index.js
@@ -10,32 +10,32 @@ app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
 });
 app.use((req, res, next) => {
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
-res.header(
-"Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept"
-);
-next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 app.use(express.json());
 
 const routes = require('./routes/routes');
 app.use('/api', routes);
 app.listen(PORT, () => {
-console.log(`Server Started at ${PORT}`)
+  console.log(`Server Started at ${PORT}`)
 })
 // Obtendo os parametros passados pela linha de comando
-var userArgs = process.argv.slice(2);
-var mongoURL = userArgs[0];
+
+var mongoURL = process.env.MONGO_URI;
 //Configurando a conexao com o Banco de Dados
 var mongoose = require('mongoose');
 mongoose.connect(mongoURL);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', (error) => {
-console.log(error)
+  console.log(error)
 })
 db.once('connected', () => {
-console.log('Database Connected');
+  console.log('Database Connected');
 })
